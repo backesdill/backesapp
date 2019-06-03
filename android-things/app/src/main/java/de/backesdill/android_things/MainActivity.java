ffcdf6546ceddfca54cfbd2e226de1aa55de78d0
kurjon <jonas.kurz@hex-factory.de>
2019-06-03 21:51:40 +0200

    new feature: reading temperature over i2c

--- android-things/app/src/main/java/de/backesdill/android_things/MainActivity.java
+++ android-things/app/src/main/java/de/backesdill/android_things/MainActivity.java
@@ -16,6 +16,14 @@ import de.backesdill.helper.NetDB;
 
 import android.content.Intent;
 
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.I2cDevice;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Skeleton of an Android Things activity.
  * <p>
@@ -36,11 +44,21 @@ import android.content.Intent;
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  */
 public class MainActivity extends Activity {
+    // debug
     private ListStorage mConsoleOutput;
+    // network
     private NetDB mNetDB;
+    // backesfest
     private TextView tvBitburgerCount;
     private TextView tvKirnerCount;
     private BackesFestData mBfData;
+    // gpio
+    private static final String GPIO_NAME = "BCM21";
+    private Gpio gpio;
+    // I2C
+    private static final String I2C_DEVICE_NAME  = "I2C1";
+    private static final int I2C_ADDRESS = 0x10;
+    private I2cDevice mDevice;
 
 
     @Override
@@ -54,18 +72,54 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mConsoleOutput = new ListStorage();
-        mConsoleOutput.add(false,"MainActivity on ActivityCreated");
+        findGpioPorts();
+        //gpioInit();
+        findI2CPorts();
+        i2cInit();
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
+        mConsoleOutput.add(false,"onStart");
         getActionBar().setTitle("Backes-Fest II - Bieranzeige");
 
         tvBitburgerCount = findViewById(R.id.tvBitburgerCount);
         tvKirnerCount = findViewById(R.id.tvKirnerCount);
 
+        /*Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // create network module
+                mBfData = new BackesFestData();
+                try {
+                    mNetDB = NetDB.getNetDB();
+                } catch (Exception e) {
+                    mConsoleOutput.add(false,"MainActivity getNetDB Exception " + e);
+                    callFatalError();
+                }
+
+                if (mNetDB != null) {
+                    mNetDB.setBackesFestCb(new BackesFestReceiverCallback() {
+                        @Override
+                        public void onReceive(BackesFestData bfData) {
+                            mConsoleOutput.add("FDisplayBackesFest onReceive()");
+
+                            mBfData.kirner = bfData.kirner;
+                            mBfData.bitburger = bfData.bitburger;
+
+                            updateGui();
+                        }
+                    });
+                    mBfData = mNetDB.getBackesFestData();
+                }
+                updateGui();
+
+            }
+        }, 10000);*/
+/*
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -96,16 +150,128 @@ public class MainActivity extends Activity {
                 updateGui();
 
             }
-        }, 10000);
+        }, 10000);*/
+
+        //gpioDoSomething();
+
+        i2cDoSomething();
+
+        mConsoleOutput.add(false,"onStart finished");
 
     }
 
     @Override
     public void onStop() {
         super.onStop();
-
+        mConsoleOutput.add(false,"onStop");
         //mNetDB.resetBackesFestCb();
+
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //mConsoleOutput.add(false,"onDestroy");
+        //gpioDeInit();
+        i2cDeInit();
+
+    }
+
+
+    private void findGpioPorts(){
+        PeripheralManager manager = PeripheralManager.getInstance();
+        List<String> portList = manager.getGpioList();
+        if (portList.isEmpty()) {
+            mConsoleOutput.add(true , "No GPIO port available on this device.");
+        } else {
+            mConsoleOutput.add(false, "List of available ports: " + portList);
+        }
+    }
+
+
+    private void gpioInit(){
+
+        // Attempt to access the GPIO
+        try {
+            PeripheralManager manager = PeripheralManager.getInstance();
+            gpio = manager.openGpio(GPIO_NAME);
+        } catch (IOException e) {
+            mConsoleOutput.add( true, "Unable to access GPIO" + e);
+        }
+    }
+
+    private void gpioDeInit(){
+
+        if (gpio != null) {
+            try {
+                gpio.close();
+                gpio = null;
+            } catch (IOException e) {
+                mConsoleOutput.add(false, "Unable to close GPIO" + e);
+            }
+        }
+    }
+
+    private void gpioDoSomething(){
+        try {
+            // Initialize the pin as a high output
+            gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            // Low voltage is considered active
+            gpio.setActiveType(Gpio.ACTIVE_HIGH);
+
+            // Toggle the value to be LOW
+            gpio.setValue(true);
+        } catch (IOException e) {
+            mConsoleOutput.add(false, "gpioDoSomething: Unable to set GPIO" + e);
+        }
+    }
+
+    private void findI2CPorts(){
+        PeripheralManager manager = PeripheralManager.getInstance();
+        List<String> deviceList = manager.getI2cBusList();
+        if (deviceList.isEmpty()) {
+            mConsoleOutput.add(true, "No I2C bus available on this device.");
+        } else {
+            mConsoleOutput.add(false, "List of available devices: " + deviceList);
+        }
+    }
+
+    private void i2cInit(){
+
+        // Attempt to access the I2C device
+        try {
+            PeripheralManager manager = PeripheralManager.getInstance();
+            mDevice = manager.openI2cDevice(I2C_DEVICE_NAME, I2C_ADDRESS);
+        } catch (IOException e) {
+            mConsoleOutput.add( true, "Unable to access I2C device " + e);
+        }
+    }
+
+    private void i2cDeInit(){
+        if (mDevice != null) {
+            try {
+                mDevice.close();
+                mDevice = null;
+            } catch (IOException e) {
+                mConsoleOutput.add( true, "Unable to close I2C device"+ e);
+            }
+        }
+
+    }
+
+    private void i2cDoSomething(){
+        byte[] buffer = new byte[6];
+
+
+        try {
+            mDevice.read( buffer, 6);
+            mConsoleOutput.add(false, "i2cDoSomething: " + Arrays.toString(buffer));
+            mConsoleOutput.add(false, "i2cDoSomething: " + new String(buffer));
+        } catch (IOException e) {
+            mConsoleOutput.add(false, "i2cDoSomething: Unable to read. " + e);
+        }
+    }
+
 
     private void updateGui(){
         if (mBfData.bitburger != Integer.parseInt(tvBitburgerCount.getText().toString())){
